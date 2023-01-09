4612a83ae5f259c58d05c7a689100b6d253611af
ameba23 <ameba23@systemli.org>
2023-01-09 12:23:55 +0100

    Doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
 //! Derive a silly animal name from a 32 byte hash or key
-//!
+//! ```
+//! let result = key_to_animal::key_to_name(&[
+//!     132, 122, 1, 1, 1, 1, 1, 1, 32, 2, 2, 2, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 3, 4, 4, 5, 6,
+//!     4, 5, 6, 3, 2,
+//! ]);
+//! assert_eq!(result, "greyishLemur".to_string());
+//! ```
 
 include!(concat!(env!("OUT_DIR"), "/words.rs"));
 
 /// Given a 32 byte buffer, return an animal name
+/// ```
+/// let result = key_to_animal::key_to_name(&[
+///     132, 122, 1, 1, 1, 1, 1, 1, 32, 2, 2, 2, 3, 4, 5, 6, 7, 8, 2, 3, 4, 5, 3, 4, 4, 5, 6,
+///     4, 5, 6, 3, 2,
+/// ]);
+/// assert_eq!(result, "greyishLemur".to_string());
+/// ```
 pub fn key_to_name(input: &[u8; 32]) -> String {
     let mut buf = [0u8; 8];
 
