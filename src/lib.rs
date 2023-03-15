c4a0ccbc8fd22697dde393349a34f94a08a3c820
peg <ameba23@systemli.org>
2023-03-15 13:51:05 +0100

    Handle usize of different lengths

--- src/lib.rs
+++ src/lib.rs
@@ -18,12 +18,13 @@ include!(concat!(env!("OUT_DIR"), "/words.rs"));
 /// assert_eq!(result, "greyishLemur".to_string());
 /// ```
 pub fn key_to_name(input: &[u8; 32]) -> String {
-    let mut buf = [0u8; 8];
+    const BUFSIZE: usize = (usize::BITS / 8) as usize;
+    let mut buf = [0u8; BUFSIZE];
 
-    buf.copy_from_slice(&input[8..16]);
+    buf.copy_from_slice(&input[BUFSIZE..BUFSIZE * 2]);
     let adjective = ADJECTIVES[usize::from_le_bytes(buf) % ADJECTIVES.len()];
 
-    buf.copy_from_slice(&input[..8]);
+    buf.copy_from_slice(&input[..BUFSIZE]);
     let animal = ANIMALS[usize::from_le_bytes(buf) % ANIMALS.len()];
 
     format!("{}{}", adjective, animal).replace(" ", "")
